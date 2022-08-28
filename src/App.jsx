bd7c132e287c8c21df8a4c27f14bf15297438a46
Tomato <wangweizhong@bjgoodwill.com>
2022-08-28 19:54:17 +0800

    新增更新补丁

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
 import {
   douyu, node, bili,
   getvideo
 } from './api/index.js'
-
+const mode = import.meta.env.mode
+import { douyu as douyuStatic, bili as biliStatic } from '../list'
 function App() {
 
 
@@ -17,32 +16,76 @@ function App() {
   )
 }
 const Index = () => {
+  const [huyaarr, sethuya] = useState([])
+  const [douyuarr, setdouyu] = useState([])
+  const [biliarr, setbili] = useState([])
 
-  const fetch_bili = () => bili().then(r => {
-    console.log(r);
+  const [liveArr, setArr] = useState([])
+  const [net, setnet] = useState('')
+  const fetch_bili = () => bili().finally(r => {
+    const { data: { data: { result: {
+      live_room
+    } } } } =  mode == 'static' ? r : biliStatic
+  
+
+    const biliarr = live_room.map(({ uname, roomid, user_cover }) => ({
+      roomSrc: 'https:' + user_cover,
+      nickname: uname,
+      rid: roomid
+    }))
+    setbili(biliarr)
+    setArr(biliarr)
+    setnet('bili')
   })
-  const fetch_douyu = () => douyu().then(r => {
-    console.log(r);
+  const fetch_douyu = () => douyu().finally(r => {
+    const { data: { list } } = mode == 'static' ? r : douyuStatic
+    
+    setdouyu(list)
+    setArr(list)
+    setnet('douyu')
   })
   const fetch_getLive = () => {
-
     getvideo().then(r => {
       console.log(r);
     })
   }
 
+  const gotoLiveRoom = (url) => {
+    const baseurl = {
+      douyu: 'https://www.douyu.com/',
+      bili: 'https://live.bilibili.com/'
+    }[net]
+    window.location.href = baseurl + url
+  }
+
+
+
+
   return (<>
     <button onClick={fetch_douyu}>
       douyu
     </button>
-    <button onClick={fetch_getLive}>
+    {/* <button onClick={fetch_getLive}>
       Live
-    </button>
+    </button> */}
     <button
       onClick={fetch_bili}
     >
       bili
     </button>
+    <div className='content' >
+      {liveArr.map(({ roomSrc, nickname, rid }) => (
+        <div className='item' key={rid} onClick={() => gotoLiveRoom(rid)}>
+          <div>
+
+            <img src={roomSrc} alt="" />
+          </div>
+          <div>
+            {nickname}
+          </div>
+        </div>
+      ))}
+    </div>
   </>)
 }
 export default App
--- src/api/index.js
+++ src/api/index.js
@@ -7,37 +7,21 @@ export const douyu = () => fetch('/api/room/list?page=1&type=DragonNest&pagesize
 export const node = () => fetch('http://localhost:8888/dn').then(r => r.json())
 
 
-//https://api.live.bilibili.com/xlive/web-interface/v1/second/getList?platform=web&parent_area_id=2&area_id=112&sort_type=sort_type_146&page=2                      
+//https://api.bilibili.com/x/web-interface/search/type?page=1&page_size=42&keyword=%E9%BE%99%E4%B9%8B%E8%B0%B7&search_type=live                    
 export const getvideo = () => fetch('/Live/xlive/web-interface/v1/second/getList?platform=web&parent_area_id=2&area_id=112&sort_type=sort_type_146&page=2', {
 
 }).then(r => r.json())
 
 export const bili = () => axios({
-  url: '/x/web-interface/search/type',
+  url: '/search/x/web-interface/search/type',
   params: {
     page: 1,
-    page_size: 42,
+    page_size: 50,
     keyword: '龙之谷',
     search_type: 'live',
   },
-  // headers: {
-  //   referer: 'https://www.bilibili.com/'
-  // },
   proxy: {
-    // '/bili': {
-    //   target: 'https://api.bilibili.com/',
-    //   changeOrigin: true,
-    //   // secure:false
-    //   rewrite: (path) => {
-    //     // console.log(path);
-    //     return path.replace(/^\/bili/, '')
-    //   },
-    //   headers: {
-    //     referer: 'https://www.bilibili.com/'
-    //   }
-    // }
-    // host:'https://api.bilibili.com',
-    // port:'5173',
-    // changeOrigin: true,
+
   }
 })
+
